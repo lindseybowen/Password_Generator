99ee1eb3b516808b11ebb2b13139316a7160e8bf
lindsey Bowen <lindseyjbowen@gmail.com>
2020-12-06 11:42:09 -0500

    call action

--- script.js
+++ script.js
@@ -1,52 +1,82 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
 
-var pwdLength = +(prompt("How long do you want your password to be?"));
-var Numbers = confirm("Do you want numbers?");
-var lowerCase = confirm("Do you want lower case letters?");
-var upperCase = confirm("Do you want upper case letters?");
-var specialChar = confirm("Do you want special characters? i.e. ! ? etc");
+// var pwdLength = +(prompt("How long do you want your password to be?"));
+// var Numbers = confirm("Do you want numbers?");
+// var lowerCase = confirm("Do you want lower case letters?");
+// var upperCase = confirm("Do you want upper case letters?");
+// var specialChar = confirm("Do you want special characters? i.e. ! ? etc");
 
 var numbers = [1,2,3,4,5,6,7,8,9];
 var lowerCase = "abcdefghijklmnopqrstuvwxyz".split('');
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
 var special = "!@#$%^&&*!@#$)(".split('');
 
-var selectednumbers = [];
-var finalnumbers = [];
-var selectedlowerCase = [];
-var finallowerCase = [];
-var selectedupperCase =[];
-var finalupperCase =[];
-var selectedCharacters = [];
-var finalCharacters=[];
 
+//need to call action 
 function generatePassword(){
-  if(upperCase){
-    selectedupperCase = selectedupperCase.concat(upperCase);
-    return finalupperCase.join('');
-  }
-  if(specialChar){
-    selectedCharacters = selectedCharacters.concat(special);
-  
-    return finalCharacters.join('');
-  }
-  if(numbers){
-    selectedCharacters = selectedCharacters.concat(numbers);
-    return finalnumbers.join('');
-  }
-  if(lowerCase){
-    selectedlowerCase = selectedlowerCase.concat(lowerCase);
-    return selectedlowerCase.join('');
-  }
-  var array = [1,2,3,4,5,6,7,8,9]
+  var allChars =[];
+  var resulttPass= "";
+}
 
-  for(i=(array.length-1);i>0;i--){
-    array[i]=array[i]++
+var passwordLength = prompt ("How long do you want your password to be?");
+
+if(passwordLength < 8 || passwordLength > 128 ) {
+  alert("Please input a value between 8 and 128");
+
+}
+
+else {
+  if (confirm("should password have lower case?")) {
+    array.prototype.push.apply(allChars, lowerCase);
+
   }
-  for(i=0;i<=pwdLength;i++){
-    finalCharacters.push(selectedCharacters[Math.floor(Math.random() * numbers.length)])
-};
+  if (confirm("should password have upper case?")){
+    array.prototype.push.apply(allChars, upperCase);
+  }
+  if (confirm("should password have numbers?")){
+    array.prototype.push.apply(allChars, numbers);
+  }
+ if (confirm("should password have special characters?")) {
+   array.prototype.push.apply(allChars, special);
+ }
+}
+
+// var selectednumbers = [];
+// var finalnumbers = [];
+// var selectedlowerCase = [];
+// var finallowerCase = [];
+// var selectedupperCase =[];
+// var finalupperCase =[];
+// var selectedCharacters = [];
+// var finalCharacters=[];
+
+// function generatePassword(){
+//   if(upperCase){
+//     selectedupperCase = selectedupperCase.concat(upperCase);
+//     return finalupperCase.join('');
+//   }
+//   if(specialChar){
+//     selectedCharacters = selectedCharacters.concat(special);
+  
+//     return finalCharacters.join('');
+//   }
+//   if(numbers){
+//     selectedCharacters = selectedCharacters.concat(numbers);
+//     return finalnumbers.join('');
+//   }
+//   if(lowerCase){
+//     selectedlowerCase = selectedlowerCase.concat(lowerCase);
+//     return selectedlowerCase.join('');
+//   }
+//   var array = [1,2,3,4,5,6,7,8,9]
+
+//   for(i=(array.length-1);i>0;i--){
+//     array[i]=array[i]++
+//   }
+//   for(i=0;i<=pwdLength;i++){
+//     finalCharacters.push(selectedCharacters[Math.floor(Math.random() * numbers.length)])
+// };
 
 // Write password to the #password input
 function writePassword() {
